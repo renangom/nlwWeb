da0b9a243137ba9bf0b463d047810ff4ba317acf
Renan <renan23eduardo@gmail.com>
2022-09-14 14:10:10 -0300

    Create initial landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,70 @@
-import { useState } from 'react'
-
+import './styles/main.css'
+import logo from './assets/Logo.png'
+import { MagnifyingGlass, MagnifyingGlassPlus } from 'phosphor-react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      Olá mundo
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-8 ">
+      <img src={logo} />
+
+      <h1 className='text-6xl text-white font-black mt-10'> Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'> duo </span> está aqui</h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='../src/assets/game1.png' alt=''/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 block mt-1'> 4 Anúncios </span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='../src/assets/game2.png' alt=''/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 block mt-1'> 4 Anúncios </span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='../src/assets/game3.png' alt=''/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 block mt-1'> 4 Anúncios </span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='../src/assets/game4.png' alt=''/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 block mt-1'> 4 Anúncios </span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='../src/assets/game5.png' alt=''/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 block mt-1'> 4 Anúncios </span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='../src/assets/game6.png' alt=''/>
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 block mt-1'> 4 Anúncios </span>
+          </div>
+        </a>
+      </div>
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-2'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center '>
+          <div>
+            <strong className='text-2xl text-white font-black'> Não encontrou seu duo? </strong>
+            <span className='text-zinc-400 block'> Publique um anúncio para encontrar novos players </span>
+          </div>
+          <button className='py-3 px-4 gap-3 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
